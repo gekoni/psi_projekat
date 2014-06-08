9aeb9f6e74028c2f2f3cffb53ec5e4bad06cbaa2
Slobodan Milicevic <ms133063m@student.etf.rs>
2014-06-08 17:20:21 +0200

    bla

--- Enciklopedija/application/views/uspesnaregistracija_view.php
+++ Enciklopedija/application/views/uspesnaregistracija_view.php
@@ -1,3 +1,4 @@
 <br/>
+<a href="template.php"></a>
 <p>Uspesno ste podneli zahtev za registraciju!</p>
-<p>Bicete obavesteni na email kada vasa registracija bude odobrena od strane administratora.</p>
+<p>Vasa registracija ceka odobrenje od strane administratora.</p>
